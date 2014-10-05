cdff425a92c4f2551c32a4953d865887f6189893
pefido <pedrofilipeduraes@hotmail.com>
2014-10-05 12:17:35 +0100

    codeblocks

--- CodeBlock.java
+++ CodeBlock.java
@@ -1,3 +1,15 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class CodeBlock {
 
+public  Deque<String> myStack= new LinkedList<String>();
+	
+	public void add(String instrucao){
+		myStack.add(instrucao);
+	}
+	
+	public String pop(){
+		return myStack.poll();
+	}
 }
