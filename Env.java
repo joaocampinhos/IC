2e9b40149223a1a221112a46bca104a92fa1b553
pefido <pedrofilipeduraes@hotmail.com>
2014-10-19 15:22:39 +0100

    Env.java

--- Env.java
+++ Env.java
@@ -1,17 +1,94 @@
+import java.util.*;
+
 public class Env {
+  Env upper;
+  Vector<Assoc> alist;
+
+  class Assoc {
+  String id;
+        IValue v;
+  
+  public Assoc(String s, IValue vi)
+  {
+    id = s;
+    v = vi;
+        }
+  
+  String getId() 
+  {
+      return id;
+        }
+
+  IValue getValue() 
+  {
+      return v;
+        }
+    }
+
+  public class UndeclaredIdentifier extends Exception {
+    String id;
+
+    public UndeclaredIdentifier(String s)
+      {
+        id = s;
+      }
+  }
+
+  public class IdentifierDeclaredTwice extends Exception {
+    String id;
+
+    public IdentifierDeclaredTwice(String s)
+      {
+        id = s;
+      }
+  }
+
   public Env() {
-    
+    upper = null;
+    alist = new Vector<Assoc>(5,1);
   }
+
+  private Env(Env up){
+    upper = up;
+  }
+
   public Env beginScope() {
-    return new Env();
+    upper = new Env(upper);
+    return upper;
   }
+
   public Env endScope() {
-    return new Env();
+    upper = upper.getUpper();
+    return upper;
   }
-  public IValue find(String id) {
-    return new IntValue(3);
+
+  public IValue find(String id) throws UndeclaredIdentifier{
+    Iterator<Assoc> al = alist.iterator();
+            while (al.hasNext()) 
+    {   
+        Assoc a = al.next();
+        if (a.getId().equals(id))
+      return a.getValue();
+    }
+      if (upper != null) 
+    return upper.find(id) ;
+    else throw new UndeclaredIdentifier(id);
   }
-  public void assoc(String id, IValue v) {
+
+  private Env getUpper(){ 
+      return upper;
   }
+
+  void assoc(String id, IValue v) throws IdentifierDeclaredTwice{ 
+      Iterator<Assoc> al = alist.iterator();
+            while (al.hasNext()) {   
+        Assoc a = al.next();
+        if (a.getId().equals(id))
+      throw new IdentifierDeclaredTwice(id);
+    }
+      alist.addElement(new Assoc(id,v));
+  }
+
+  
 }
 
