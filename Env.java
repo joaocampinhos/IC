c8c8436c2a3f92506bb53aa4c86352f7564fec20
joaocampinhos <joao.campinhos@gmail.com>
2014-10-19 15:35:18 +0100

    whitespace

--- Env.java
+++ Env.java
@@ -5,42 +5,42 @@ public class Env {
   Vector<Assoc> alist;
 
   class Assoc {
-  String id;
-        IValue v;
-  
-  public Assoc(String s, IValue vi)
-  {
-    id = s;
-    v = vi;
-        }
-  
-  String getId() 
-  {
-      return id;
-        }
+    String id;
+    IValue v;
 
-  IValue getValue() 
-  {
-      return v;
-        }
+    public Assoc(String s, IValue vi)
+    {
+      id = s;
+      v = vi;
     }
+
+    String getId()
+    {
+      return id;
+    }
+
+    IValue getVal
+    {
+      return v;
+    }
+  }
 
   public class UndeclaredIdentifier extends Exception {
     String id;
 
     public UndeclaredIdentifier(String s)
-      {
-        id = s;
-      }
+    {
+      id = s;
+    }
   }
 
   public class IdentifierDeclaredTwice extends Exception {
     String id;
 
     public IdentifierDeclaredTwice(String s)
-      {
-        id = s;
-      }
+    {
+      id = s;
+    }
   }
 
   public Env() {
@@ -64,31 +64,31 @@ public class Env {
 
   public IValue find(String id) throws UndeclaredIdentifier{
     Iterator<Assoc> al = alist.iterator();
-            while (al.hasNext()) 
-    {   
-        Assoc a = al.next();
-        if (a.getId().equals(id))
-      return a.getValue();
+    while (al.hasNext())
+    {
+      Assoc a = al.next();
+      if (a.getId().equals(id))
+        return a.getValue();
     }
-      if (upper != null) 
-    return upper.find(id) ;
+    if (upper != null)
+      return upper.find(id) ;
     else throw new UndeclaredIdentifier(id);
   }
 
-  private Env getUpper(){ 
-      return upper;
+  private Env getUpper(){
+    return upper;
   }
 
-  void assoc(String id, IValue v) throws IdentifierDeclaredTwice{ 
-      Iterator<Assoc> al = alist.iterator();
-            while (al.hasNext()) {   
-        Assoc a = al.next();
-        if (a.getId().equals(id))
-      throw new IdentifierDeclaredTwice(id);
+  void assoc(String id, IValue v) throws IdentifierDeclaredTwice{
+    Iterator<Assoc> al = alist.iterator();
+    while (al.hasNext()) {
+      Assoc a = al.next();
+      if (a.getId().equals(id))
+        throw new IdentifierDeclaredTwice(id);
     }
-      alist.addElement(new Assoc(id,v));
+    alist.addElement(new Assoc(id,v));
   }
 
-  
+
 }
 
