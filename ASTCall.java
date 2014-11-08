130a813e9f0798ae72831d37a3f9706bebb74478
joaocampinhos <joao.campinhos@gmail.com>
2014-11-08 14:25:17 +0000

    ASTCall. closes #28

--- ASTCall.java
+++ ASTCall.java
@@ -1,16 +1,30 @@
 import java.util.*;
 
 public class ASTCall implements ASTNode {
-  Vector<ASTId> vs;
+  Vector<ASTNode> vs;
   ASTNode body;
 
   public IValue eval(Env e) throws TypeError, Env.IdentifierDeclaredTwice, Env.UndeclaredIdentifier {
-    e.beginScope();
-    //TODO: fazer isto!
-    return null;
+    IValue f = body.eval(e);
+    if (f.typeOf() == IValue.VType.FUN) {
+      FunValue fun = (FunValue)f;
+      Env en = e.beginScope();
+      Vector<String> vv = fun.getParameter();
+      Iterator<ASTNode> it = vs.iterator();
+      Iterator<String> iv = vv.iterator();
+      while(iv.hasNext()) {
+        String d = iv.next();
+        ASTNode a = it.next();
+        IValue b = a.eval(e);
+        en.assoc(d, b);
+      }
+      IValue r = fun.getBody().eval(en);
+      en.endScope();
+      return r;
+    } else throw new TypeError();
   }
 
-  public ASTCall(Vector<ASTId> vec, ASTNode b) {
+  public ASTCall(Vector<ASTNode> vec, ASTNode b) {
     vs = vec;
     body  = b;
   }
