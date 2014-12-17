f9a32015aabbb2e1b17e59b17b377ade404552fa
joaocampinhos <joao.campinhos@gmail.com>
2014-12-17 23:26:27 +0000

    hmmm

--- ASTGetRec.java
+++ ASTGetRec.java
@@ -2,19 +2,29 @@ import java.util.*;
 
 public class ASTGetRec implements ASTNode {
   ASTNode t;
-  String id;
+  //String id;
+  ASTNode id;
 
   public IValue eval(Env e) throws TypeError, Env.IdentifierDeclaredTwice, Env.UndeclaredIdentifier, UndefinedField{
 
     IValue r = t.eval(e);
+    IValue idv = id.eval(e);
+
+    System.out.println(r.typeOf());
+    System.out.println(idv.typeOf());
 
     if (r.typeOf() == IValue.VType.REC) {
+
+      IdValue name = (IdValue)idv;
 
       RecValue rec = (RecValue)r;
 
       Env ee = rec.getEnv();
 
-      ASTNode el = rec.getParameter(id);
+      //ASTNode el = rec.getParameter(id);
+      System.out.println(name.toString());
+      ASTNode el = rec.getParameter(name.toString());
+
 
       IValue res = el.eval(ee);
 
@@ -24,7 +34,8 @@ public class ASTGetRec implements ASTNode {
 
   }
 
-  public ASTGetRec(String id, ASTNode t) {
+  public ASTGetRec(ASTNode id, ASTNode t) {
+  //public ASTGetRec(String id, ASTNode t) {
     this.t = t;
     this.id = id;
   }
